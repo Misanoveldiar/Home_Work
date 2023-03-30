3232802c52fc5feb9afa7e0f79e0d93c857f61c8
Eldiar <misanoveldiar@gmail.com>
2023-03-30 14:51:09 +0600

    js
    js

---
+++ index.js/index.js
@@ -0,0 +1,43 @@
+
+////////////////////////////////////////////Home Work
+1
+//let name = ('Eldior')
+//console.log(name);
+2
+//let age = '22'
+//console.log(age)
+3
+//let IsAdult = prompt('Введите ваш возраст')
+//if (IsAdult == 18) { console.log(true) }
+//else { console.log(false) }
+4
+//let questionName = prompt('Ваше Имя?')
+//let userName
+//alert('userName')
+//console.log(userName)
+5
+//let email = confirm('Согласны ли вы получать рекламные уведомление на Email')
+//if (email == true) {
+//console.log('Доступно')
+//}
+//else { console.log('Недоступно') }
+
+//let password = prompt('YOUR PASSWORD')
+//if (password == 0000) { console.log('Доступ разрешен') }
+//else { console.log('Доступ запрешен') }
+7
+//let number = prompt("Придумайте число!",)
+//if (number % 2 == 0) { console.log('Четное число') }
+//else { console.log('Нечетное число') }
+8
+//let color = prompt('Какого цвета машина ?')
+//if (color == 'Красный' || color == 'красный') { console.log('Красный') }
+//else { console.log('Некрасный') }
+9
+10
+//let grade = prompt('Оценице Фильм')
+//if (grade == 'a' || grade == 'A') { console.log('Отлично') }
+//else if (grade == 'b' || grade == 'B') { console.log('Хорошо') }
+//else if (grade == 'c' || grade == 'C') { console.log('Удолетворительно') }
+//else if (grade == 'd' || grade == 'D') { console.log('Неудолдетворительно') }
+//else if (grade == 'f' || grade == 'F') { console.log('Плохо') }
